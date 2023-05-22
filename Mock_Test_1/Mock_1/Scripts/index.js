bf5cfc3b99d6861c52241b28ab7332a238296576
email <tarunmehto71@gmail.com>
2023-05-22 17:23:52 +0530

    done

--- Mock_Test_1/Mock_1/Scripts/index.js
+++ Mock_Test_1/Mock_1/Scripts/index.js
@@ -1,1 +1,33 @@
-// Write code related to Home page here 
+let form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let obj = {
+    name: form.name.value,
+    start_data: form.start.value,
+    end_data: form.end.value,
+    priority: form.priority.value,
+  };
+  // console.log(obj)
+
+  
+
+  let store = localStorage.getItem("task-list");
+
+  if (store) {
+    let data = localStorage.getItem("task-list");
+    let pasred = JSON.parse(data);
+    pasred.push(obj);
+
+    localStorage.setItem("task-list", JSON.stringify(pasred));
+  } else {
+    localStorage.setItem("task-list", JSON.stringify([obj]));
+  }
+
+
+
+});
+
+
+
+
